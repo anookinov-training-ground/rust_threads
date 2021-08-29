61b3d97707826d25431a85559c0f508e39f24aa8
Alexander Nookinov <anookinov@gmail.com>
2021-08-29 12:40:50 +0700

    send multiple values and see the receiver waiting

--- src/main.rs
+++ src/main.rs
@@ -8,11 +8,31 @@ fn main() {
     thread::spawn(move || {
         let val = String::from("hi");
         tx.send(val).unwrap();
-        println!("val is {}", val); // error as val has been moved after it's sent to another thread
+        // println!("val is {}", val); // error as val has been moved after it's sent to another thread
     });
 
     let received = rx.recv().unwrap();
     println!("Got: {}", received);
+
+    let (tx, rx) = mpsc::channel();
+
+    thread::spawn(move || {
+        let vals = vec![
+            String::from("hi"),
+            String::from("from"),
+            String::from("the"),
+            String::from("thread"),
+        ];
+
+        for val in vals {
+            tx.send(val).unwrap();
+            thread::sleep(Duration::from_secs(1));
+        }
+    });
+
+    for received in rx {
+        println!("Got: {}", received);
+    }
 
     let handle = thread::spawn(|| {
         for i in 1..10 {
