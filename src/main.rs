8448b66aed3cf2b1312a9c18dff6a56e0ed74524
Alexander Nookinov <anookinov@gmail.com>
2021-08-29 19:38:56 +0700

    add sharing a Mutex<T> between multiple threads

--- src/main.rs
+++ src/main.rs
@@ -12,6 +12,24 @@ fn main() {
     }
 
     println!("m = {:?}", m);
+
+    let counter = Mutex::new(0);
+    let mut handles = vec![];
+
+    for _ in 0..10 {
+        let handle = thread::spawn(move || {
+            let mut num = counter.lock().unwrap();
+
+            *num += 1;
+        });
+        handles.push(handle);
+    }
+
+    for handle in handles {
+        handle.join().unwrap();
+    }
+
+    println!("Result: {}", *counter.lock().unwrap());
     
     let (tx, rx) = mpsc::channel();
 
